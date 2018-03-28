d5f8d1b4ce793cfbdb7d2ba0f32ed1c2f99a8c02
Ryan Hu <ryanhuuu@gmail.com>
2018-03-28 22:02:23 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,30 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = {}
+  data.each do |t1key, t1val|
+    if t1key == :color || t1key == :gender
+      t1val.each do |t2key, t2val|
+        t2val.each do |name|
+          if !result.has_key?(name)
+            result[name] = {}
+            result[name][t1key] = [t2key.to_s]
+          elsif result[name][t1key]
+            result[name][t1key] << t2key.to_s
+          else
+            result[name][t1key] = [t2key.to_s]
+          end
+        end
+      end
+      
+# :lives starts here
+
+    else
+      t1val.each do |t2key, t2val|
+        t2val.each do |name|
+          result[name][:lives] = [t2key.to_s]
+        end
+      end
+    end
+
+  end
+  return result
 end
